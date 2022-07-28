d49df772c6dfbce1cef075b995668c6ea1d02edd
ethanlee <seungwoonlee90@gmail.com>
2022-07-28 19:06:31 +0900

    setting Router

--- src/App.js
+++ src/App.js
@@ -3,15 +3,23 @@ import Navbar from './components/Navbar';
 import GridInfo from './components/Grid';
 import data from './data';
 import { useState } from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   let [shoes] = useState(data);
   return (
     <div className="App">
     <Navbar />
-    { shoes.map(function(a, i){
+    <Link to="/">Home</Link>
+    <Link to="/detail">Detail</Link>
+    <Routes>
+      <Route path="/" element={
+        shoes.map(function(a, i){
       return <GridInfo shoes={shoes[i]}/>
-    })}
+    })}/>
+      <Route path="/detail" />
+    </Routes>
+
     </div>
   );
 }
